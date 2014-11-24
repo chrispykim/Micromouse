0ece5d0c3f362d53e3af40c252062c833ec9501e
Cathy Chen <chen.catherine92@gmail.com>
2014-11-23 17:18:23 -0800

    I added the derivative (D) control variable and changed a few function and variable names to be more clear about our PID control process!

--- PID_pseudocode.c
+++ PID_pseudocode.c
@@ -1,26 +1,28 @@
-void correct_path(some variables) 
+void PID(some variables) 
 {
 
   static int previous_error = 0;
   static int SP;                    // SP (set point) is value we want process to be
   static int PV;                    // PV (process value) is  actual value
-  static int Kp = someConstant, Ki = someOtherConstant;    // constants for PI stuff (TBD)
-  static int error, P, I=0;     // error variables
-  int total=0; 
+  static int error;	            // error variables 
+  static int P, I, D = 0;           // proportional(P), integral (I), & derivative(D) control action variables
+  static int Kp = constantP, Ki = constantI, Kd = constantD;    // gain constants for PID controller 
+  int controllerOutput = 0; 
   
   SP = something;
-  PV = getError();
+  PV = getError();                  // error obtained from IR sensors
   
   error = SP-PV; // error value on which PI actions take place
   
-  if (error != 0) { // 
+  if (error != 0) { 
     
-    P = error * Kp;
-    I = (I+error) * Ki;    
+    P = error * Kp;      // proportional control (P)
+    I = takeIntegral(error) * Ki;  // integral action control (I) 
+    D = takeDerivative(error)*Kd;  // derivative action control (D)
     previous_error = error;
-    total = (P+I);
+    controllerOutput = P+I+D;
   }
 
-  doSomethingToController(total);
+  setDriveSpeedRight(controllerOutput);
   // adjust voltage to motors
 }
