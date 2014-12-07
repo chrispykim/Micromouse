0ed19726a331011f3308a847327979c7f580a12b
chrispykim <chris.kim342@gmail.com>
2014-12-07 02:05:25 -0800

    arduino part9

--- PID.c
+++ PID.c
@@ -4,7 +4,8 @@ void setup() {
   static int ScaleFactor = SOME_CONSTANT;  // idk
   static int Kp = constantP, Ki = constantI, Kd = constantD;  // gain constants for PID controller 
   static int Threshold = SOME_OTHER_CONSTANT;  // preventing integral windup (idk what to put rn)
-  static int previous_error = 0;  // stuff for derivative
+  static int previous_error = analogRead(LeftSensor) - analogRead(RightSensor); // error value to initialize
+  static const int DELAY = 15;
   // pin #'s to be changed later
   LeftSensor = 1;
   RightSensor = 2;
@@ -16,6 +17,7 @@ void setup() {
 
 void loop() {
 
+  delay(DELAY);
   PID(); 
 }
 
@@ -41,24 +43,23 @@ void PID() {
     
   P = Error;  // proportional control (P)
   I = Sum;  // integral action control (I)
-  D = (Error-previous_error)/dt;  // derivative action control (D)
+  D = (Error - previous_error)/DELAY;  // derivative action control (D)
   previous_error = Error;  // save current value for next time
   controllerOutput = P*Kp + I*Ki + D*Kd;  // weighted sum for motor
-  controllerOutput = controllerOutput*ScaleFactor; // scale Drive to be in the range 0-255 
+  controllerOutput *= ScaleFactor; // scale Drive to be in the range 0-255 
 
-  if (controllerOutput < 0){  // Check which direction to go.
-    
-    digitalWrite (Direction,LOW);  // Direction is some pin#
-  }
-  else { // depending on the sign of Error
-    
-    digitalWrite (Direction,HIGH);
-  }
-  
   if (abs(controllerOutput)>255) {
     
     controllerOutput=255;
   }
-  
-  analogWrite (Motor,controllerOutput); 
+
+  if (controllerOutput < 0){  // Check which motor to change.
+    
+    analogWrite(MotorLeft,abs(controllerOutput));  // not really sure what to do here
+    // what do we tell right motor to do???
+  }
+  else { // depending on the sign of Error
+    
+    analogWrite(MotorRight,abs(controllerOutput);
+  }
 }
