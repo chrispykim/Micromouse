6b92fe0a067d921b041b9118c4993c94c866946e
Chris Kim <chris.kim342@gmail.com>
2015-05-11 20:31:27 -0700

    encoders

--- moveMotors/moveMotors.ino
+++ moveMotors/moveMotors.ino
@@ -1,22 +1,78 @@
+#include <Encoder.h>
+
+Encoder left(17,18);
+Encoder right(19,20);
 /* move motors constant speed */
 
-int motorPin = 3;   // potentiometer connected to analog pin 3
+int leftMotorPWM = 3; 
+int leftMotorForward = 5;
+int leftMotorBackward = 4;
+int rightMotorPWM = 9;
+int rightMotorForward = 8;
+int rightMotorBackward = 7;
+int STBY = 6;
 int led = 13;
 int maxVal = 255;
 int wait = 250;
+unsigned int counter = 0;
 
 void setup() {
-  //pinMode(motorPin, OUTPUT);   // sets the pin as output
-  //pinMode(led, OUTPUT);
+  Serial.begin(9600);
+  pinMode(STBY, OUTPUT);  
+  pinMode(led, OUTPUT);
+  pinMode(leftMotorPWM, OUTPUT);
+  pinMode(leftMotorForward, OUTPUT);
+  pinMode(leftMotorBackward, OUTPUT);
+  pinMode(rightMotorPWM, OUTPUT);
+  pinMode(rightMotorForward, OUTPUT);
+  pinMode(rightMotorBackward, OUTPUT);
+  digitalWrite(STBY, HIGH); // just need this
 }
 
+long positionLeft = -999;
+long positionRight = -999;
 
-
-void loop() {
-  /*digitalWrite(led,HIGH);
+void loop() {  
+  // blink
+  /*digitalWrite(led, HIGH);
   delay(wait); 
-  digitalWrite(led,LOW);
+  digitalWrite(led, LOW);
   delay(wait);*/
-  analogWrite(ledPin, maxVal/2);  // analogRead values go from 0 to 1023, analogWrite values from 0 to 255
+  bool forward = true;
+  setMotor(maxVal/4, forward, forward);
+  
+  long newLeft, newRight;
+  newLeft = left.read();
+  newRight = right.read();
+  
+  if (newLeft != positionLeft || newRight != positionRight) {
+    counter++;
+    Serial.print("Count = ");
+    Serial.print(counter);
+    Serial.print(", Left = ");
+    Serial.print(newLeft);
+    Serial.print(", Right = ");
+    Serial.print(newRight);
+    Serial.println();
+    positionLeft = newLeft;
+    positionRight = newRight;
+  }
+  // if a character is sent from the serial monitor,
+  // reset both back to zero.
+  if (Serial.available()) {
+    Serial.read();
+    Serial.println("Reset both encoders to zero");
+    left.write(0);
+    right.write(0);
+  }
 }
 
+void setMotor(int val, boolean left, boolean right) {
+  digitalWrite(leftMotorForward, left);
+  digitalWrite(leftMotorBackward, !left);
+  digitalWrite(rightMotorForward, right);
+  digitalWrite(rightMotorBackward, !right);
+  
+  analogWrite(leftMotorPWM, val);  
+  analogWrite(rightMotorPWM, val);
+}
--- sensorTest/sensorTest.ino
+++ sensorTest/sensorTest.ino
@@ -5,41 +5,53 @@
 // Pin 13 LED
 // Pin 14 Analog in for sensor
 int led = 13;
-int leftSensor = 14;
-
+int leftSensor = 16;
+int frontSensor = 15;
+int rightSensor = 14;
 
 void setup() {
   //Serial.begin(38400);
   Serial.begin(9600);
   pinMode(led, OUTPUT);
   pinMode(leftSensor, INPUT);
+  pinMode(frontSensor, INPUT);
+  pinMode(rightSensor, INPUT);
 }
 
-int sensorValue;
-int wait = 250;
+int sensorValue1;
+int sensorValue2;
+int sensorValue3;
+int wait = 500;
 int counter = 0;
 float total = 0.0;
 
 void loop() {
   counter++;
-  sensorValue = analogRead(leftSensor);
-  float voltage = sensorValue * (5.0/1023.0);
-  total += sensorValue;
+  sensorValue1 = analogRead(rightSensor);
+  sensorValue2 = analogRead(frontSensor);
+  sensorValue3 = analogRead(leftSensor);
+  //float voltage = sensorValue * (5.0/1023.0);
+  //total += sensorValue;
   /* blink while taking readings */
   digitalWrite(led,HIGH);
   delay(wait); 
   digitalWrite(led,LOW);
   delay(wait);
   /* print out average of 10 readings */
-  Serial.println(sensorValue);
-  //Serial.println(voltage);
-  //Serial.print(",");
-  if (counter == 10) {
+  Serial.print("14: ");
+  Serial.print(sensorValue1);
+  Serial.print(", 15: ");
+  Serial.print(sensorValue2);
+  Serial.print(", 16: ");
+  Serial.print(sensorValue3);
+  Serial.println();
+  
+  /*if (counter == 10) {
     float avg = total/10.0;
     counter = 0;
     total = 0;
-    Serial.print(avg);
-    Serial.println(",");
+    Serial.print("avg =");
+    Serial.println(avg);
     //delay(5000);
-  }
+  }*/
 }
