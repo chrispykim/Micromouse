d8939b6dbe5a5f6e32a4cd1e520090da19145334
Chris Kim <chris.kim342@gmail.com>
2015-05-13 21:07:22 -0700

    encoders

--- moveMotors/moveMotors.ino
+++ moveMotors/moveMotors.ino
@@ -1,9 +1,5 @@
-#include <Encoder.h>
-
-Encoder left(17,18);
-Encoder right(19,20);
-/* move motors constant speed */
-
+int leftEnc = 17;
+int rightEnc = 19;
 int leftMotorPWM = 3; 
 int leftMotorForward = 5;
 int leftMotorBackward = 4;
@@ -14,7 +10,14 @@ int STBY = 6;
 int led = 13;
 int maxVal = 255;
 int wait = 250;
-unsigned int counter = 0;
+
+volatile unsigned int lpulse=100000;                      // width of left and right encoder pulses in uS
+volatile unsigned int rpulse=100000;                      // width of left and right encoder pulses in uS
+volatile unsigned int lcount,rcount;                      // left and right pulse counters to measure distance
+volatile byte lflag;                                      // flag indicates when left  encoder changes states
+volatile byte rflag;                                      // flag indicates when right encoder changes states
+volatile unsigned long ltime;                             // remembers time of left  encoders last state change in uS
+volatile unsigned long rtime;                             // remembers time of right encoders last state change in uS
 
 void setup() {
   Serial.begin(9600);
@@ -27,6 +30,10 @@ void setup() {
   pinMode(rightMotorForward, OUTPUT);
   pinMode(rightMotorBackward, OUTPUT);
   digitalWrite(STBY, HIGH); // just need this
+  digitalWrite(leftEnc,1);                               // enable pullup resistor for left  encoder sensor
+  digitalWrite(rightEnc,1);                               // enable pullup resistor for right encoder sensor
+  attachInterrupt(0,Lencoder,CHANGE);                     // trigger left  encoder ISR on state change
+  attachInterrupt(1,Rencoder,CHANGE);                     // trigger right encoder ISR on state change
 }
 
 long positionLeft = -999;
@@ -38,33 +45,32 @@ void loop() {
   delay(wait); 
   digitalWrite(led, LOW);
   delay(wait);*/
+  
   bool forward = true;
-  setMotor(maxVal/4, forward, forward);
-  
-  long newLeft, newRight;
-  newLeft = left.read();
-  newRight = right.read();
-  
-  if (newLeft != positionLeft || newRight != positionRight) {
-    counter++;
-    Serial.print("Count = ");
-    Serial.print(counter);
-    Serial.print(", Left = ");
-    Serial.print(newLeft);
-    Serial.print(", Right = ");
-    Serial.print(newRight);
-    Serial.println();
-    positionLeft = newLeft;
-    positionRight = newRight;
-  }
-  // if a character is sent from the serial monitor,
-  // reset both back to zero.
-  if (Serial.available()) {
-    Serial.read();
-    Serial.println("Reset both encoders to zero");
-    left.write(0);
-    right.write(0);
-  }
+  setMotor(maxVal/5, forward, forward);
+  delay(1000);
+  setMotor(0, forward, forward);
+  delay(3000);
+  Serial.print(lcount);
+  Serial.print(", ");
+  Serial.println(rcount);
+}
+
+//======================================================= ISR for left encoder =======================================================
+void Lencoder()
+{
+  lpulse=micros()-ltime;                               // time between last state change and this state change
+  ltime=micros();                                      // update ltime with time of most recent state change
+  lcount++;                                            // increment left motor distance counter
+}
+
+
+//======================================================= ISR for right encoder ======================================================
+void Rencoder()
+{
+  rpulse=micros()-rtime;                               // time between last state change and this state change
+  rtime=micros();                                      // update ltime with time of most recent state change
+  rcount++;                                            // increment right motor distance counter
 }
 
 void setMotor(int val, boolean left, boolean right) {
